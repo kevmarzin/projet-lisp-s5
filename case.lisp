935e9f9d441f19a355ae831fbb753bf68d46c2e0
Adrien Bounader <adrien.bounader@etu.u-bordeaux.fr>
2015-11-26 14:47:32 +0000

    case.lisp supprimé

--- case.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :sudoku)
-
-(defclass case ()
-  ((ligne :initarg :ligne :accessor lig)
-   (colonne :initarg :colonne :accessor col)
-   (contenu :initarg :contenu :accessor cont))
-  (:documentation "case"))
