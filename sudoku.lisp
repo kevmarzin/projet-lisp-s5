ee7bd1e288b7c3d684d395219ed53cababbf7c84
Adrien Bounader <adrien.bounader@etu.u-bordeaux.fr>
2015-12-01 20:09:20 +0100

    simplification de la boucle principale

--- sudoku.lisp
+++ sudoku.lisp
@@ -23,21 +23,26 @@
   `(- (char-int ,caractere) (char-int #\A)))
     
 
-;;Fonction principale
+;; Fonction principale
 (defun sudoku (grid)
   (init-grille *grille* grid) ; Initialisation de la grille
 
-  (let ((jouer t))
+  (let ((jouer t)
+	(saisie-incorrect nil)
+	(colonne-saisie nil)
+	(ligne-saisie nil))
     (while jouer ; Boucle de jeu
-      (print *grille*)
-      (let ((saisie-incorrect t))
-	(while saisie-incorrect
-	  (format t "C L? ")
-	  (let ((colonne (read-char))
-		(ligne (read)))
-	    (format t "Value? ")
-	    (if (setf saisie-incorrect (not (modifier-case *grille*
-							   (1- ligne)
-							   (conversion-char-to-int colonne)
-							   (read))))
-		(format t "Informations saisies invalides reessayer ~%"))))))))
+      (print *grille*) ; Affichage de la grille
+      (setf saisie-incorrect t)
+      (while saisie-incorrect ; Boucle qui redemande la saisie si elle est incorrecte
+	(format t " C L? ") ; Demande des coordonnées de la case à modifer
+	(setf colonne-saisie (read-char)) ; Récupération de la colonne
+	(setf ligne-saisie (read)) ; Récupération de la ligne
+	(format t "Value? ") ; Demande de la valeur
+
+	; Vérification de la validité de la saisie, on continu la demande si elle est incorrect
+	(if (setf saisie-incorrect (not (modifier-case *grille*
+						       (1- ligne-saisie)
+						       (conversion-char-to-int colonne-saisie)
+						       (read))))
+	    (format t "Informations saisies invalides reessayer ~%"))))))
